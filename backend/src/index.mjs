33873a6166cf91ec69f82d4589799b784518e82a
crapu <crapu@example.com>
2025-07-28 15:07:32 +0300

    added images routes and controllers

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import envPath from './config.mjs';
 import express from 'express';
 import usersRoutes from './routes/usersRoutes.mjs';
+import imagesRoutes from './routes/imagesRoutes.mjs';
 
 dotenv.config({ path: envPath });
 
@@ -10,6 +11,7 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use('/users', usersRoutes);
+app.use('/images', imagesRoutes);
 
 app.listen(PORT, () => {
   console.log(`listenting on port ${PORT}`);
