154b0298b312c3fa1c97fbd84cac4731619155c3
crapu <crapu@example.com>
2025-07-30 16:08:50 +0300

    function/multer

--- backend/src/controllers/imagesController.mjs
+++ backend/src/controllers/imagesController.mjs
@@ -1,6 +1,6 @@
 export const uploadImage = (req, res) => {
   // res.status(202).json({ message: 'image uploaded successfully' });
-  res.status(202).json({ message: req.body });
+  res.status(202).json({ message: req.file });
 };
 
 export const deleteImage = (req, res) => {
--- backend/src/routes/imagesRoutes.mjs
+++ backend/src/routes/imagesRoutes.mjs
@@ -1,9 +1,12 @@
 import express from 'express';
+import multer from 'multer';
 import { deleteImage, uploadImage } from '../controllers/imagesController.mjs';
 
 const router = express.Router();
+const upload = multer({ dest: 'images/' });
 
-router.post('/upload', uploadImage);
+// "file" name must be the name from key name in payload
+router.post('/upload', upload.single('file'), uploadImage);
 router.delete('/delete/:id', deleteImage);
 
 export default router;
