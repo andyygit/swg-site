f55a6f7a6a9c5aee92a11238389f202366cb2688
crapu <crapu@example.com>
2025-07-31 15:17:53 +0300

    function/multer

--- backend/src/controllers/imagesController.mjs
+++ backend/src/controllers/imagesController.mjs
@@ -1,8 +1,46 @@
-export const uploadImage = (req, res) => {
-  // res.status(202).json({ message: 'image uploaded successfully' });
-  res.status(202).json({ message: req.file });
+import multer from 'multer';
+
+// user!!!!!!!!!!!!!!!!!!!!!!!!
+const username = 'cinevaundeva';
+
+// Configure storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images/');
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+    cb(null, `${username}-${uniqueSuffix}.jpg`);
+  },
+});
+
+// File filter (optional)
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg') {
+    cb(null, true);
+  } else {
+    cb(new Error('Unsupported file type'), false);
+  }
 };
 
-export const deleteImage = (req, res) => {
+// "file" name must be the name from key name in the client request payload
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+}).single('file');
+
+export const uploadImage = (req, res, next) => {
+  upload(req, res, (err) => {
+    if (err instanceof multer.MulterError) {
+      return next({ status: 500, message: err.message });
+    } else if (err) {
+      return next({ status: 500, message: err.message });
+    }
+    res.status(202).json({ message: req.file });
+  });
+};
+
+export const deleteImage = (req, res, next) => {
   res.status(200).json({ message: 'image deleted successfully' });
+  console.log(req.params.id);
 };
--- backend/src/routes/imagesRoutes.mjs
+++ backend/src/routes/imagesRoutes.mjs
@@ -1,23 +1,9 @@
 import express from 'express';
-import multer from 'multer';
 import { deleteImage, uploadImage } from '../controllers/imagesController.mjs';
-// user!!!!!!!!!!!!!!!!!!!!!!!!
-const username = 'cinevaundeva';
 
 const router = express.Router();
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'images/');
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
-    cb(null, `${username}-${uniqueSuffix}.jpg`);
-  },
-});
-const upload = multer({ storage: storage });
 
-// "file" name must be the name from key name in the client request payload
-router.post('/upload', upload.single('file'), uploadImage);
+router.post('/upload', uploadImage);
 router.delete('/delete/:id', deleteImage);
 
 export default router;
