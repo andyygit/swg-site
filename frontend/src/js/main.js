946a57402bc2484777cb3a1c3e3e8ee8e1dae171
crapu <crapu@example.com>
2025-08-27 15:00:00 +0300

    play/main.js

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,26 +1,22 @@
-const getWeather = () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve('sunny');
-    }, 2000);
+const getData = async (url) => {
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(`Fetch response NOK: ${res.status}`);
+    }
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const fetchAndRenderData = async () => {
+  const data = await getData('https://jsonplaceholder.typicode.com/users');
+  data.forEach((item) => {
+    let { name, email } = item;
+    console.log(`${name} - ${email}`);
   });
 };
 
-const getWeathericon = (weather) => {
-  console.log('function 2');
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(`${weather} with icon added`);
-    }, 2000);
-  });
-};
-
-const onSuccess = (data) => {
-  console.log(`Success: ${data}`);
-};
-
-const onError = (err) => {
-  console.log(`Error: ${err}`);
-};
-
-getWeather().then(getWeathericon).then(onSuccess).catch(onError);
+// fetchAndRenderData();
